c8f6ffc3f8fc41c3f64e98d3bf8467ff9d81c660
Nautiluss <nautiluss@localhost.localdomain>
2015-05-07 10:20:18 -0500

    Pruba4

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,15 +2,18 @@ var app = angular.module('starter.controllers', []);
 
 
 app.controller('EventosCtrl', function($scope,$timeout){
-	$scope.eventos = eventos.slice(0,14);
+	$scope.eventos = eventos.slice(0,12);
 	$scope.haveMoreData = true;
+	
 	$scope.loadMore = function() {
 		$timeout(function(){
-			$scope.eventos = $scope.eventos.concat(eventos.slice(0,14));
+			$scope.eventos = $scope.eventos.concat(eventos.slice(0,12));
 			$scope.$broadcast('scroll.infiniteScrollcomplete');
 		}, 2000);
 	};
 });
+
+
 
 
 
